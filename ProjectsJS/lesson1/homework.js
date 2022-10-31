5bc0874c5c92a9368c3fafe41022ca3f1d0f64a3
Norejon <oreonorej@gmail.com>
2022-10-31 23:27:22 +0200

    hw1 js

---
+++ ProjectsJS/lesson1/homework.js
@@ -0,0 +1,42 @@
+// part1
+
+// let a = "hello";
+// let b = "owu";
+// let c = " com";
+// let d = "ua";
+// let e = 1;
+// let f = 10;
+// let j = -999;
+// let g = 123;
+// let h = 3.14;
+// let t = 2.7;
+// let l = 16;
+// let r = true;
+// let s = false;
+//  console.log(a,b,c,d,e,f,g,j,h,t,l,r,s);
+
+
+//part2
+
+// let firstName = "Sozanskiy";
+// let middleName = "Mykola";
+// let lastName = "Yaroslavowich";
+//
+// let PIB = firstName +" "+middleName+" "+lastName;
+// console.log(PIB);
+
+//part3
+
+// let a = 100;
+// let b = '100';
+// let c = true;
+//
+// console.log("a is - "+typeof(a) , ", b is - "+typeof(b),", c is - "+typeof (c));
+
+//part4
+
+// let Imya = prompt("Write your Imya");
+// let PoBatkowi = prompt("Write your PoBat'kowi");
+// let Age = prompt("Write your Age");
+//
+// console.log(Imya+" "+ PoBatkowi+" "+Age);
