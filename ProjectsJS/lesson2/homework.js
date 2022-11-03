8e189632b437fd2b9676f914c648228462243b53
Norejon <oreonorej@gmail.com>
2022-11-03 16:58:01 +0200

    hw2 js

---
+++ ProjectsJS/lesson2/homework.js
@@ -0,0 +1,57 @@
+// part 1
+
+// let arr = [1,'hello',32,true,21,false,23.99,'dog',true,13];
+// console.log(arr[0],arr[1],arr[2],arr[3],arr[4],arr[5],arr[6],arr[7],arr[8],arr[9]);
+
+//part 2
+
+// let bookOne = {
+//   title: "Orel karpatskih gir",
+//     pageCount: 480,
+//     genre: "ekshn"
+// };
+// let bookTwo = {
+//     title: "Ozero spogadiv",
+//     pageCount: 397,
+//     genre: "roman"
+// };
+// let bookThree = {
+//     title: "Galactyka prygod",
+//     pageCount: 999,
+//     genre: "fantastika"
+// };
+
+//part 3
+
+// let bookOne = {
+//     title: "Orel karpatskih gir",
+//     pageCount: 480,
+//     genre: "ekshn",
+//     authors: [{name: "Anton", age:37},{name: "Olga", age:28}]
+// };
+// let bookTwo = {
+//     title: "Ozero spogadiv",
+//     pageCount: 397,
+//     genre: "roman",
+//     authors: [{name: "Anton", age:37},{name: "Olga", age:28}]
+// };
+// let bookThree = {
+//     title: "Galactyka prygod",
+//     pageCount: 999,
+//     genre: "fantastika",
+//     authors: [{name: "Anton", age:37},{name: "Olga", age:28}]
+// };
+
+//part 4
+
+// let users = [user1={name: "Olha", username:"Terminator",password:"Aloren228339RR"},
+//     user2={name: "Roman", username:"romko22",password:"SomebodyOnce"},
+//     user3={name: "Petro", username:"Perets",password:"Perchigo12"},
+//     user4={name: "Sofia", username:"sandbox",password:"Garpia350"},
+//     user5={name: "Vasyl", username:"vasylisk",password:"vasyl110379"},
+//     user6={name: "Gavrylo", username:"BredPit",password:"NeNQts99ds00w"},
+//     user7={name: "Maria", username:"mashaCat",password:"EnterToOblivionDor"},
+//     user8={name: "Adam", username:"adam123",password:"adam123Adam123"},
+//     user9={name: "Nastya", username:"JustSmile",password:"ahahaNonPassword3"},
+//     user10={name: "Anton", username:"antonino",password:"Pizzalover007"}];
+// console.log(users[0].password,users[1].password,users[2].password,users[3].password,users[4].password,users[5].password,users[6].password,users[7].password,users[8].password,users[9].password);
