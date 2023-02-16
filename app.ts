8b3de89a6bb43567d8b8e6deedb57f3edc01dd7d
NorejOn <oreonorej@gmail.com>
2023-02-16 19:11:29 +0200

    hw8 typescript v2

--- app.ts
+++ app.ts
@@ -37,28 +37,42 @@
 // }
 
 //*************************|Виконання Part1|***********************************
+interface  Core{
+    reuse_count: number,
+    status: string
+}
+interface Cores {
+    flight: number,
+    core: Core
+}
 
-type link ={article_link:string|number,video_link:string|number};
-type rocket={rocket_name:string,
-    first_stage:{cores:[{
-            flight: number, core:
-                {
-                    reuse_count: number,
-                    status: string
-                }
-        }]},
-    second_stage: {payloads:[
-            {payload_type:string,
-                payload_mass_kg:number,
-                payload_mass_lbs:number}
-        ]}
+interface Payloads {
+    payload_type: string,
+    payload_mass_kg: number,
+    payload_mass_lbs: number
+}
+
+interface Link  {
+    article_link: string | number,
+    video_link: string | number };
+interface First_stage{
+    cores: Cores[]
+}
+interface Second_stage {
+    payloads: Payloads[]
+}
+interface Rocket  {
+    rocket_name: string,
+    first_stage: First_stage,
+    second_stage: Second_stage
 };
-interface IStarship{
+
+interface IStarship {
     mission_name: string,
-    launch_date_local:string,
+    launch_date_local: string,
     launch_site: object,
-    links:link
-    rocket:rocket
+    links: Link
+    rocket: Rocket
 }
 
 
@@ -87,28 +101,34 @@ interface IStarship{
 // incAge(user, 2)
 
 
-
 //***************************|Виконання Part2|************************
 
-const user:({name:string,age:number,gender:string}) = {
-    name:"Max",
-    age:18,
-    gender:'male'
+interface User {
+    name: string,
+    age: number,
+    gender: string
+
 }
 
-function sum(a:number,b:number):number{
-    return a+b
+const user: User = {
+    name: "Max",
+    age: 18,
+    gender: 'male'
 }
 
-function showSum(a:number,b:number):void{
+function sum(a: number, b: number): number {
+    return a + b
+}
+
+function showSum(a: number, b: number): void {
     console.log(a + b);
 }
 
-function incAge(someUser:{age?:number}, inc:number):object{
-    someUser.age+=inc
+function incAge(someUser: User, inc: number): User {
+    someUser.age += inc
     return someUser
 }
 
 console.log(sum(1, 2));
-showSum(2,3)
+showSum(2, 3)
 incAge(user, 2)
